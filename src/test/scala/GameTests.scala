790bba32027f8890247997b51641839cbac2d00b
vorou <vorou@skbkontur.ru>
2014-01-30 00:02:48 +0600

    add eq

--- src/test/scala/GameTests.scala
+++ src/test/scala/GameTests.scala
@@ -9,7 +9,7 @@ class GameTests extends JUnitSuite with Matchers with TableDrivenPropertyChecks 
         new Game(board)
     }
 
-    def AssertState(board: Array[Array[Char]], state: Char) {
+    def AssertState(board: Array[Array[Char]], state: Char) = {
         val game = createGame(board)
 
         val actual = game.getState
@@ -17,7 +17,7 @@ class GameTests extends JUnitSuite with Matchers with TableDrivenPropertyChecks 
         actual shouldBe state
     }
 
-    def AssertState(boards: TableFor1[Array[Array[Char]]], state: Char) {
+    def AssertState(boards: TableFor1[Array[Array[Char]]], state: Char) = {
         forAll(boards) {
             (board) =>
                 AssertState(board, state)
@@ -25,13 +25,13 @@ class GameTests extends JUnitSuite with Matchers with TableDrivenPropertyChecks 
     }
 
     @Test
-    def state_noMarks_isUnknown() {
+    def state_noMarks_isUnknown() = {
         val emptyBoard = Array.fill(3, 3)('-')
         AssertState(emptyBoard, '-')
     }
 
     @Test
-    def state_noThrees_isUnknown() {
+    def state_noThrees_isUnknown() = {
         AssertState(Table(
             "board",
             Array(
@@ -63,7 +63,7 @@ class GameTests extends JUnitSuite with Matchers with TableDrivenPropertyChecks 
     }
 
     @Test
-    def state_threeOsInRow_isOwon() {
+    def state_threeOsInRow_isOwon() = {
         AssertState(Table(
             "board",
             Array(
